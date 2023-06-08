f662b31c10f31aa7d30e57a9bf58ae2e998d547b
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-08 12:20:46 +0200

    add function in js for send message

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+
  * 
  * 
  */
@@ -175,12 +180,22 @@ createApp({
                 }
             ],
         }
-    ]
+    ],
+    activeUser:0,
+    newMessage:'',
   };
   },
   methods: {
-
-    
-
-  },
+    activeIndexUpdate(index) {
+        this.activeIndex = index
+    },
+    sendNewMessage() {
+        if ((this.newMessage !== '') && (this.newMessage.trim().length !== 0)) {
+            newMessageAdd = { date: this.currentDateWithFormat(), message: this.newMessage, status: 'sent' };
+            this.contacts[this.activeIndex].messages.push(newMessageAdd);
+            this.newMessage = ''
+            newMessageReceived = setTimeout(() => this.newMessageReceived(), 1000);
+        }
+    },
+  }
 }).mount('#app');
